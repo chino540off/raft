30283b1edcee0928ae2ea3256c8593185a9ad55b
Olivier Detour <olivier.detour@scality.com>
2017-01-09 14:27:40 +0100

    add comments, and add template tests.

--- include/raft/store/fs.hh
+++ include/raft/store/fs.hh
@@ -23,53 +23,95 @@ class filesystem
    * @brief API
    */
   public:
+    /**
+     * @brief Create Key
+     *
+     * @param k the key
+     * @param v the value
+     *
+     * @return false on error
+     */
     bool
     c(Key const & k, Value const & v)
     {
-      fs::path      k_path = _base_path / k;
-      std::ofstream  stream(k_path, std::ios::binary);
+      fs::path        k_path = key_path(k);
+      std::ofstream   stream(k_path, std::ios::binary);
 
+      // FIXME
       stream.write(reinterpret_cast<char const *>(&v), sizeof (Value));
       stream.close();
 
       return true;
     }
 
+    /**
+     * @brief Read Key
+     *
+     * @param k the key
+     *
+     * @return the value
+     */
     Value
     r(Key const & k) const
     {
-      Value         value;
-      fs::path      k_path = _base_path / k;
+      fs::path        k_path = key_path(k);
+      Value           value;
 
       if (!fs::exists(k_path))
         throw std::out_of_range("key not found");
 
-      std::ifstream  stream(k_path, std::ios::binary);
+      std::ifstream   stream(k_path, std::ios::binary);
 
+      // FIXME
       stream.read(reinterpret_cast<char *>(&value), sizeof (Value));
       stream.close();
 
       return value;
     }
 
+    /**
+     * @brief Update key
+     *
+     * @param k the key
+     * @param v the new value
+     *
+     * @return false on error
+     */
     bool
     u(Key const & k, Value const & v)
     {
-      fs::path      k_path = _base_path / k;
-      std::ofstream  stream(k_path, std::ios::binary);
+      fs::path        k_path = key_path(k);
+      std::ofstream   stream(k_path, std::ios::binary);
 
+      // FIXME
       stream.write(reinterpret_cast<char const *>(&v), sizeof (Value));
       stream.close();
 
       return true;
     }
 
+    /**
+     * @brief Delete key
+     *
+     * @param k the key
+     */
     void
     d(Key const & k)
     {
-      fs::path      k_path = _base_path / k;
+      fs::path        k_path = key_path(k);
 
       fs::remove(k_path);
+    }
+
+  private:
+    fs::path
+    key_path(Key const & k) const
+    {
+      std::stringstream ss_k;
+
+      ss_k << k;
+
+      return _base_path / ss_k.str();
     }
 
   private:
--- tests/tests_store.cc
+++ tests/tests_store.cc
@@ -4,21 +4,25 @@
 #include <raft/store/memory.hh>
 #include <raft/store/fs.hh>
 
-template <typename T>
-void
-utest(T store)
+template <typename T,
+          typename Key,
+          typename Value>
+void utest(T store,
+           Key const & k,
+           Value const & v1,
+           Value const & v2)
 {
-  store.c("k", 21);
-  assert(store.r("k") == 21);
+  store.c(k, v1);
+  assert(store.r(k) == v1);
 
-  store.u("k", 42);
-  assert(store.r("k") == 42);
+  store.u(k, v2);
+  assert(store.r(k) == v2);
 
-  store.d("k");
+  store.d(k);
 
   try
   {
-    store.r("k");
+    store.r(k);
     assert(false);
   }
   catch (std::out_of_range & e)
@@ -28,14 +32,39 @@ utest(T store)
 
 }
 
+struct MyKey
+{
+  int k;
+};
+
+template <typename ostream>
+inline ostream &
+operator<<(ostream & os, MyKey const & k)
+{
+  os << k.k;
+  return os;
+}
+
+struct MyValue
+{
+  int v;
+
+  bool
+  operator==(MyValue const & v)
+  {
+    return this->v == v.v;
+  }
+};
+
 int
 main(void)
 {
-  raft::store::memory<std::string, int> store1;
-  raft::store::filesystem<std::string, int> store2;
-
-  utest(store1);
-  utest(store2);
+  utest(raft::store::memory<std::string, int>(), "k", 42, 21);
+  utest(raft::store::filesystem<std::string, int>(), "k", 42, 21);
+  utest(raft::store::filesystem<int, int>(), 101, 42, 21);
+  utest(raft::store::filesystem<MyKey, MyValue>(),
+        MyKey({ 101 }),
+        MyValue({ 42 }), MyValue({ 21 }));
 
   return 0;
 }
